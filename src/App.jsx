648f089a2fb3c9349e956c8a388967ed2e05164e
Vasyl <vasapanov721@gmail.com>
2024-03-08 10:47:23 +0100

    Buxs fixed

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,7 @@ function App() {
   }
 
   const startGame = () => {
+    setIsRulesOpen(false);
     setIsGameStarted(!isGameStarted);
   }
 
--- src/components/Rules.jsx
+++ src/components/Rules.jsx
@@ -4,7 +4,7 @@ export default function Rules() {
     return (
         <div className='pt-4 w-screen p-5'>
             <div className='text-3xl font-semibold text-center'>Rules</div>
-            <div className=' list-item text-xl ml-3 pt-4'>Use sentences like: <br /> 122) " furthermore- более того " </div>
+            <div className=' list-item text-xl ml-3 pt-4'>Use sentences like: <br /> 122)furthermore- более того </div>
             <div className=' list-item text-xl ml-3 pt-4'>Use new sentences on new line</div>
             <div className=' list-item text-xl ml-3 pt-4'>Sign "-" must be ONE in sentence, between words</div>
             <div className=' list-item text-xl ml-3 pt-4'>Instead of "-" in words use "_"</div>
